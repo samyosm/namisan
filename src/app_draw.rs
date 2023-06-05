5c8d6e1cf6fa8c9f861e3e05953a1bd349829f53
Samy <samy.osmium@gmail.com>
2023-06-05 19:24:43 -0400

    implement(AppState): items automatically set from pwd (#2)

--- src/app_draw.rs
+++ src/app_draw.rs
@@ -10,6 +10,12 @@ use crate::app::AppState;
 
 pub fn draw_app(app: &AppState, f: &mut Frame<CrosstermBackend<io::Stdout>>) {
     let size = f.size();
-    let block = Block::default().title(app.pwd).borders(Borders::ALL);
+    let block = Block::default()
+        .title(
+            app.pwd()
+                .to_str()
+                .expect("couldn't get current workding directory from app state"),
+        )
+        .borders(Borders::ALL);
     f.render_widget(block, size);
 }
--- src/app_update.rs
+++ src/app_update.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use crossterm::event::{KeyCode, KeyEvent};
 
 use crate::app::{AppCommand, AppState};
@@ -5,7 +7,8 @@ use crate::app::{AppCommand, AppState};
 pub fn update_app(app: &mut AppState, event: &KeyEvent) -> AppCommand {
     match event.code {
         KeyCode::Char('p') => {
-            app.pwd = "Hello, world!";
+            let path = app.pwd().parent().unwrap_or(Path::new("/"));
+            app.change_pwd(path.to_path_buf());
         }
         KeyCode::Char('q') => return AppCommand::Quit,
         _ => {}
